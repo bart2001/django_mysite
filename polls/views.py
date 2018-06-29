890d1edec0cc3b694ef46554ccf3b0aa10466619
bart2001 <bart2001@naver.com>
2018-06-30 01:21:53 +0900

    This is first commit

---
+++ polls/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
+from .models import Question
+from django.shortcuts import get_object_or_404
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
